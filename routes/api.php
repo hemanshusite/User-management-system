c5caada8ee990fc9ca6b02d62184dbbd23343ee5
Hemanshu Solanki <85437130+hemanshusite@users.noreply.github.com>
2026-03-17 10:42:58 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,8 @@ Route::prefix('v1')->group(function () {
     
     // Users routes
     Route::get('/users', [UserController::class, 'index'])->name('users.index');
-    Route::post('/users', [UserController::class, 'store'])->name('users.store');
-    Route::get('/users/{id?}', [UserController::class, 'show'])->name('users.show');
-    Route::put('/users/{id?}', [UserController::class, 'update'])->name('users.update');
-    Route::delete('/users/{id?}', [UserController::class, 'destroy'])->name('users.destroy');
+    Route::post('/users/create', [UserController::class, 'store'])->name('users.store');
+    Route::get('/users/view/{id?}', [UserController::class, 'show'])->name('users.show');
+    Route::put('/users/update/{id?}', [UserController::class, 'update'])->name('users.update');
+    Route::delete('/users/delete/{id?}', [UserController::class, 'destroy'])->name('users.destroy');
 });
